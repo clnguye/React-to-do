f49d0654b392f3e942f13d22f33e73a512062848
clnguye <clnguye@yahoo.com>
2023-05-01 11:20:16 -0500

    Initial Commit

---
+++ src/NewTodoForm.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+export function NewTodoForm({ onSubmit }) {
+    const [newItem, setNewItem] = useState("")
+    //const [newTodo, setNewTodo] = useState([])
+    
+    function handleSubmit(e) {
+        e.preventDefault()
+        if (newItem === "") return
+
+        onSubmit(newItem) 
+
+        // setTodos(currentTodos => {
+        //     return [
+        //         ...currentTodos,
+        //         {id: crypto.randomUUID, title: newItem, completed: false },
+        //     ]
+        // })
+
+        setNewItem("")
+    }
+
+return (
+    <form onSubmit={handleSubmit} onDoubleClick={handleSubmit} className="new-item-form">
+        <div className="form-row">
+            <label htmlFor="item">New Item</label>
+            <input 
+                value={newItem} 
+                onChange={e => setNewItem(e.target.value)}
+                //onKeyUp={e => setNewItem(e.target.value)}
+                type="text" 
+                id="item"
+            />
+        </div>
+        <button className="btn">Add</button>
+    </form>
+)
+}
